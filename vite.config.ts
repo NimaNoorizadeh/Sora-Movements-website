43778e68edeb041a2fb8ac84dd5734bb31a8a82f
Sora Project <project@soramovement.example>
2026-09-14 13:51:06 +0330

    Deploy site with GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins: [react()], server: { watch: { ignored: ['**/review/**', '**/.npm-cache/**'] } } });
+
+export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/Sora-Movements-website/' : '/',
+  plugins: [react()],
+  server: { watch: { ignored: ['**/review/**', '**/.npm-cache/**'] } },
+});
